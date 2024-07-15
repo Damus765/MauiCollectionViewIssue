c81e3cf08c2e60a11dcad4e4a9e6f25cbbbf7eba
Robin Grenet <robin.grenet@wanadoo.fr>
2024-07-15 20:32:49 +0200

    Ajoutez des fichiers projet.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiCollectionViewIssue
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
